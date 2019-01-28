875abf96c08bbbca76d0efb436efef567c425625
Luke Sweeney <llamicron@Lukes-Air.domain>
2019-01-27 18:25:04 -0600

    Makes the print output look a lot better

--- parse.py
+++ parse.py
@@ -1,4 +1,5 @@
 import re
+import operator
 
 def parse_request(request_string):
     """
@@ -59,44 +60,50 @@ for req_string in req_strings:
 
 
 
-# # We now have a list of request objects with keys and values instead of strings. We can start counting things to report stats.
+# We now have a list of request objects with keys and values instead of strings. We can start counting things to report stats.
 
-# # The first one is easy
-# # Total number of requests; print the length of the requests list
-# print(len(reqs) + ' total requests')
+print('-- Request counts --')
+# The first one is easy
+# Total number of requests; print the length of the requests list
+print(str(len(reqs)) + ' total requests')
 
 
-# # Lets get a list of the timestamps, cutting out any that are == '' (errors in the logfile)
-# dates = [d['timestamp'][0:11] for d in reqs if d['timestamp'] != '']
+print('\n-- Requests by Date --')
+# Lets get a list of the timestamps, cutting out any that are == '' (errors in the logfile)
+dates = [d['timestamp'][0:11] for d in reqs if d['timestamp'] != '']
 
-# # Split the dates from '24/Oct/1994' to ['24', 'Oct', '1994'] for all the items
-# dates = [d.split('/') for d in dates]
+# Split the dates from '24/Oct/1994' to ['24', 'Oct', '1994'] for all the items
+dates = [d.split('/') for d in dates]
 
-# # This creates a new dictionary with month names as keys, and the day numbers in a list as the value, ex.
-# # {
-# #     'Oct': [24, 24, 24, 25, 25, 26, 26, 27, 27, 27],
-# #     'Nov': [1, 2, 3, 3, 3, 4]
-# # }
-# # We retain duplicates so we can count them, that's how many requests were made each day
-# monthly = {}
-# for date in dates:
-#     if date[1] not in monthly.keys():
-#         monthly[date[1]] = []
-#     monthly[date[1]].append(date[0])
+# This creates a new dictionary with month names as keys, and the day numbers in a list as the value, ex.
+# {
+#     'Oct': [24, 24, 24, 25, 25, 26, 26, 27, 27, 27],
+#     'Nov': [1, 2, 3, 3, 3, 4]
+# }
+# We retain duplicates so we can count them, that's how many requests were made each day
+monthly = {}
+for date in dates:
+    if date[1] not in monthly.keys():
+        monthly[date[1]] = []
+    monthly[date[1]].append(date[0])
 
-# # For each month
-# for month, days in monthly.items():
-#     # Monthly
-#     # Just count the number of items in the `days` list
-#     print(str(len(days)) + ' requests in ' + month)
+# For each month
+for month, days in monthly.items():
+    # Monthly
+    # Just count the number of items in the `days` list
+    print('\n-- ' + month + ' --')
+    print(str(len(days)) + ' total requests')
 
-#     # Daily
-#     # For each day, print the amount of times that day occurs in the `days` list
-#     for day in set(days):
-#         print(month + ': ' + str(days.count(day)) + ' requests on the ' + str(day))
+    # Daily
+    # For each day, print the amount of times that day occurs in the `days` list
+    for day in set(days):
+        print(str(days.count(day)) + ' requests on ' + month + ' ' + str(day))
 
-#     # Weekly
-#     # TODO: Do this
+    # Weekly
+    # TODO: Do this
+
+
+print('\n-- Request Status Codes -- ')
 
 
 # Get a list of all the status codes, again retaining duplicates
@@ -115,3 +122,27 @@ redirected = [r for r in status_codes if r[0] == '3']
 percentage_redirected = round(len(redirected) / len(status_codes), 3)
 print("% of redirected requests (3xx status code): " + str(percentage_redirected * 100) + "%")
 
+
+print('\n-- Files --')
+
+# Get a list of requested files with duplicates
+files = [f['file'] for f in reqs]
+
+# Count how many times each file was requested with a dictionary
+file_counts = {}
+for fi in files:
+    if fi not in file_counts.keys():
+        file_counts[fi] = 0
+    file_counts[fi] += 1
+
+
+# Get the key with the highest value
+most_requested_file = max(file_counts.items(), key=operator.itemgetter(1))[0]
+print('most requested file is: ' + most_requested_file)
+
+# Least requested files
+# There are a lot with only 1 request, just print the amount instead of a name
+least_requested_files = [f[0] for f in file_counts.items() if f[1] == 1]
+print('There are ' + str(len(least_requested_files)) + ' files that were requested 1 time')
+
+print('\nDone\n')
